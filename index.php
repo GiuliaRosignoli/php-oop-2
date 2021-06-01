b4d8f4b9b42f809e9c03559f6cb2f659a9eed9f3
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-06-01 17:45:51 +0200

    .

--- index.php
+++ index.php
@@ -18,17 +18,28 @@ class Customer {
     public $id_user = 001;
     protected $first_name = 'Giulia';
     protected $lastname = ' Rosignoli';
-    protected $membership = 'Date to be inserted';
+    protected $membership = ' (Date to be inserted)';
 
-
+    // Public functions
     public function introduceYourself(){
         echo 'Hello, I am ';
 
         $this->printCustomerFullName();
     }
 
+    public function membershipDet(){
+        echo 'I am a member since ';
+
+        $this->printMembershipDetails();
+    }
+
+    // Protected functions
     protected function printCustomerFullName(){
         echo $this->first_name . '' . $this->lastname;
+    }
+
+    protected function printMembershipDetails(){
+        echo $this->membership;
     }
 
 
@@ -38,7 +49,8 @@ class Customer {
 
 $user_1 = new Customer();
 echo $user_1->id_user . '<br>';
-$user_1->introduceYourself();
+$user_1-> introduceYourself() ;
+$user_1->membershipDet();
 
 
 
