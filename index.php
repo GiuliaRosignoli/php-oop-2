2b88c2d45ad8c60141765a15a617482aaa066a62
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-06-02 18:00:53 +0200

    .

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 
-class Book {
+class Book {  // General features
 public $title;
 public $author;
 public $ISBN;
@@ -11,10 +11,10 @@ protected $discount;
 
 
 
-// Constructor
+// Book Constructor
 function __construct($author, $price){
     $this->author = $author;
-    $this->price = $price;
+    $this->price = number_format($price, 2);
 }
 
 // Methods
@@ -31,7 +31,7 @@ public function discountApplied($perc){
 }
     
 
-} // Book class
+} // Book class ends here
 
 
 
@@ -63,13 +63,16 @@ class Fiction extends Book {
 
 //  1st Fiction Book instance
 $The_Picture_of_Dorian_Gray = new Fiction('Oscar Wilde', 12, 'fiction');
-echo 'This book was written by' . ' ' . $The_Picture_of_Dorian_Gray->author . '<br>';
-echo  'The reduced price of the item is €' . ' ' . $The_Picture_of_Dorian_Gray->discountApplied(10) . '<br>';
+echo 'This book was written by' . ' ' . $The_Picture_of_Dorian_Gray->author . '.' . '<br>';
+echo 'The price of the item is € ' .' ' . $The_Picture_of_Dorian_Gray->price . '.' . '<br>';
+echo  'The reduced price of the item is €' . ' ' . $The_Picture_of_Dorian_Gray->discountApplied(10) . '.' . '<br>';
 echo '<br>';
 
 // 2nd Fiction Book instance
 $Nineteen_Eighty_Four = new Fiction('George Orwell', 10, 'fiction');
-echo 'This book was written by' . ' ' . $Nineteen_Eighty_Four->author . '<br>';
+echo 'This book was written by' . ' ' . $Nineteen_Eighty_Four->author . '.' . '<br>';
+echo 'The price of the item is € ' .' ' . $Nineteen_Eighty_Four->price . '.' . '<br>';
+echo  'The reduced price of the item is €' . ' ' . $Nineteen_Eighty_Four->discountApplied(15) . '.' . '<br>';
 echo '<br>';
 
 
@@ -77,21 +80,29 @@ echo '<br>';
 
 $The_Second_Sex = new Non_fiction('Simone de Beauvoir', 20, 'philosophy');
 $price = 20;
-echo 'This book was written by' . ' ' . $The_Second_Sex->author . '<br>';
+echo 'This book was written by' . ' ' . $The_Second_Sex->author . '.' . '<br>';
+echo 'The price of the item is € ' .' ' . $The_Second_Sex->price . '.' . '<br>';
+echo  'The reduced price of the item is €' . ' ' . $The_Second_Sex->discountApplied(20) . '.' . '<br>';
 
 
 
 // Next class Customer
-class Customer {
+class Customer {  //general features
     // Properties
     public $id_user = 1;
-    protected $first_name = 'Giulia';
-    protected $lastname = ' Rosignoli';
+    protected $first_name = 'Jane';
+    protected $lastname = 'Doe';
+    public $age;
     protected $membership = ' (Date to be inserted)';
+    protected $discount = 0;
 
-    // Public functions
+    // Constructor 
+    function __construct($id_user, $age){
+    $this->id_user = $id_user;
+    $this->age = $age;
+    }
 
-   
+    // Public methods
 
     public function introduceYourself(){
         echo '<br>';
@@ -106,26 +117,61 @@ class Customer {
         $this->printMembershipDetails();
     }
 
-    // Protected functions
+    // extra discount - protected and public methods
+    protected function setExtraDiscount(){ // protected
+        $this->discount = $this->age >= 30 ? 'Congratulations, you got an extra discount!' :  'I\'m sorry, you lose :(';
+    }
+
+    public function getADiscount(){
+        $this->setExtraDiscount();
+        return $this->discount;
+    }
+
+
+    // Protected methods
     protected function printCustomerFullName(){
-        echo $this->first_name . '' . $this->lastname;
+        echo $this->first_name . ' ' . $this->lastname;
     }
 
     protected function printMembershipDetails(){
         echo $this->membership;
     }
 
+} // Customer class ends here
 
-} // Customer class
+
+class Over30 extends Customer {
+// Constructor
+function __construct($id_user, $age, $name){
+    parent::__construct($id_user, $age);
+    $this->name = $name;
+}
+
+
+} // class Over30 ends here
+
+class Under30 extends Customer {
+// Constructor
+function __construct($id_user, $age, $name){
+    parent::__construct($id_user, $age);
+    $this->name = $name;
+}
+
+
+} // class Under30 ends here
 
 
 // Instances
 
-$user_1 = new Customer();
+$user_1 = new Customer(1, 35);
 // echo $user_1->id_user . '<br>';
 $user_1-> introduceYourself();
 echo '<br>';
 $user_1->membershipDet();
+echo '<br>';
+$user_1->getADiscount();
+echo 'Different categories are foreseen within the competition, according to the age of the participants: you are' . ' ' . $user_1->age . ' , therefore' . ' ' . $user_1->getADiscount();
+
 
 
 
